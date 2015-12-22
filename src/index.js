eb9c07350dccb74fc4d9dd633124ed7f3d13c487
Zeno Zeng <zenoofzeng@gmail.com>
2015-12-22 20:06:14 +0800

    remove comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ export default function ({ types: t }) {
     return {
         visitor: {
             Program(path, file) {
-                // Inject `var CatchPromise = require('catch-promise');`
                 var catchPromiseDeclaration = t.variableDeclaration('var', [
                     t.variableDeclarator(
                         t.identifier('regeneratorRuntime'),
